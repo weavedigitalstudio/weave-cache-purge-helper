b7593c84ffb752188e2bfa8207792867f9684c21
Gareth <5170216+gbissland@users.noreply.github.com>
2024-06-30 19:16:13 +1200

    Update cache-purge-helper.php

--- cache-purge-helper.php
+++ cache-purge-helper.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Weave Cache Purge Helper
  * Plugin URI:        https://yourforkedversion.com
  * Description:       Fork of Cache Purge Helper for Weave Digital Use. Adds additional WordPress and BB, ACF hooks to trigger NGINX Helper / lscache plugin purges.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Gareth Bissland, Paul Stoute, Jordan Trask, Jeff Cleverley
  * Author URI:        https://weave.co.nz
  * Text Domain:       weave-cache-purge-helper
@@ -12,7 +12,7 @@
  * Tested up to:      5.4
  *
  * @link              https://github.com/managingwp/cache-purge-helper
- * @since             1.0.0
+ * @since             1.0.1
  * @package           weave-cache-purge-helper
  * Original Author:   Paul Stoute, Jordan Trask, Jeff Cleverley
  * Original Plugin URI: https://github.com/managingwp/cache-purge-helper
@@ -28,85 +28,85 @@
   * is activated for purging server cache.
   */
  
- function wcph_purge() {
-	 // Purge WordPress Cache
-	 $called_action_hook = current_filter();
-	 wcph_write_log('wcph - initiated');
-	 wcph_write_log('wcph - running on ' . $called_action_hook);
-	 wcph_write_log('wcph - flushing WordPress Cache first');
-	 wp_cache_flush();
-   
-	 // If nginx-helper plugin is enabled, purge cache.
-	 wcph_write_log('wcph - checking for nginx-helper plugin');
- 
-	 if ( is_plugin_active('nginx-helper/nginx-helper.php') ) {
-		 wcph_write_log('wcph - nginx-helper plugin installed, running $nginx_purger->purge_all();');
-		 global $nginx_purger;
-		 $nginx_purger->purge_all();
-	 } else {
-		 wcph_write_log('wcph - nginx-helper plugin not installed or detected');
-	 }
+function wcph_purge() {
+    // Purge WordPress Cache
+    $called_action_hook = current_filter();
+    wcph_write_log('wcph - initiated on ' . $called_action_hook);
+    wp_cache_flush();
   
-	 // If litespeed-cache plugin is enabled, purge cache.
-	 wcph_write_log('wcph - checking for litespeed-cache plugin');
- 
-	 if ( is_plugin_active('litespeed-cache/litespeed-cache.php') ) {
-		 wcph_write_log('wcph - litespeed-cache plugin installed, running do_action(\'litespeed_purge_all\');');
-		 do_action( 'litespeed_purge_all' );
-	 } else {
-		 wcph_write_log('wcph - litespeed-cache plugin not installed or detected');
-	 }
- 
-	 // End of cache_purge_helper()
-	 wcph_write_log('wcph - end of cache_purge_helper function');
- }
- 
- /** Log to WordPress Debug Log Function
-  *
-  * Log to PHP error_log if WP_DEBUG and WC_PHP_DEBUG are set!
-  *
-  */
- 
- function wcph_write_log ( $log )  {
-	 if ( WP_DEBUG === true && defined('WC_PHP_DEBUG')) {
-		 if ( is_array( $log ) || is_object( $log ) ) {
-			 error_log( print_r( $log, true ) );
-		 } else {
-			 error_log( $log );
-		 }
-	 }
- }
- 
- /*************/
- 
- // Plugin Update Hooks
- wcph_write_log('wcph - Loading WordPress core hooks');
- 
- add_action( 'upgrader_process_complete', 'wcph_purge', 10, 0 ); // After plugins have been updated
- add_action( 'activated_plugin', 'wcph_purge', 10, 0); // After a plugin has been activated
- add_action( 'deactivated_plugin', 'wcph_purge', 10, 0); // After a plugin has been deactivated
- add_action( 'switch_theme', 'wcph_purge', 10, 0); // After a theme has been changed
- 
- // Beaver Builder
- if ( defined( 'FL_BUILDER_VERSION' ) ) {
-	 wcph_write_log('wcph - Beaver Builder Hooks enabled');
-	 add_action( 'fl_builder_cache_cleared', 'wcph_purge', 10, 3 );
-	 add_action( 'fl_builder_after_save_layout', 'wcph_purge', 10, 3 );
-	 add_action( 'fl_builder_after_save_user_template', 'wcph_purge', 10, 3 );
-	 add_action( 'upgrader_process_complete', 'wcph_purge', 10, 3 );
- }
- 
- // ACF Options Page
- if ( function_exists('acf') ) {
-	 add_action( 'acf/save_post', function( $post_id ) {
-		 if ( $post_id === 'options' ) {
-			 wcph_write_log('wcph - ACF options page saved, clearing Beaver Builder cache.');
-			 // Clear Beaver Builder cache
-			 FLBuilderModel::delete_asset_cache_for_all_posts();
-			 wcph_write_log('wcph - Beaver Builder cache cleared.');
- 
-			 // Purge caches
-			 wcph_purge();
-		 }
-	 });
- }
+    // If nginx-helper plugin is enabled, purge cache.
+    if ( is_plugin_active('nginx-helper/nginx-helper.php') ) {
+        global $nginx_purger;
+        if ( isset( $nginx_purger ) ) {
+            wcph_write_log('wcph - nginx-helper plugin detected, purging cache.');
+            $nginx_purger->purge_all();
+        }
+    }
+
+    // If litespeed-cache plugin is enabled, purge cache.
+    if ( is_plugin_active('litespeed-cache/litespeed-cache.php') ) {
+        wcph_write_log('wcph - litespeed-cache plugin detected, purging cache.');
+        do_action('litespeed_purge_all');
+    }
+
+    wcph_write_log('wcph - cache purge completed.');
+}
+
+/** Log to WordPress Debug Log Function
+ *
+ * Log to PHP error_log if WP_DEBUG and WC_PHP_DEBUG are set!
+ *
+ */
+
+function wcph_write_log($log) {
+    if (WP_DEBUG === true && defined('WC_PHP_DEBUG') && WC_PHP_DEBUG === true) {
+        if (is_array($log) || is_object($log)) {
+            error_log(print_r($log, true));
+        } else {
+            error_log($log);
+        }
+    }
+}
+
+/** Initialize Plugin Hooks */
+function wcph_init_hooks() {
+    static $initialized = false;
+
+    if ($initialized) {
+        return;
+    }
+
+    $initialized = true;
+
+    // Plugin Update Hooks
+    add_action('upgrader_process_complete', 'wcph_purge', 10, 0); // After plugins have been updated
+    add_action('activated_plugin', 'wcph_purge', 10, 0); // After a plugin has been activated
+    add_action('deactivated_plugin', 'wcph_purge', 10, 0); // After a plugin has been deactivated
+    add_action('switch_theme', 'wcph_purge', 10, 0); // After a theme has been changed
+
+    // Beaver Builder
+    if (defined('FL_BUILDER_VERSION')) {
+        add_action('fl_builder_cache_cleared', 'wcph_purge', 10, 3);
+        add_action('fl_builder_after_save_layout', 'wcph_purge', 10, 3);
+        add_action('fl_builder_after_save_user_template', 'wcph_purge', 10, 3);
+        add_action('upgrader_process_complete', 'wcph_purge', 10, 3);
+    }
+
+    // ACF Options Page
+    if (function_exists('acf')) {
+        add_action('acf/save_post', function($post_id) {
+            if ($post_id === 'options') {
+                wcph_write_log('wcph - ACF options page saved, clearing Beaver Builder cache.');
+                // Clear Beaver Builder cache
+                FLBuilderModel::delete_asset_cache_for_all_posts();
+                wcph_write_log('wcph - Beaver Builder cache cleared.');
+
+                // Purge caches
+                wcph_purge();
+            }
+        });
+    }
+}
+
+// Initialize plugin hooks only once
+add_action('init', 'wcph_init_hooks');
